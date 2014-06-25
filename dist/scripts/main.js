c0e2c736169632ec5db2ac68985ba6c83dcd6758
Jonathan Dickerson <jonathandickerson@Jonathans-MacBook-Pro-4.local>
2014-06-25 09:01:28 -0400

    testing

---
+++ dist/scripts/main.js
@@ -0,0 +1,96 @@
+
+$('#allo').click(function(){
+	console.log('change color');
+
+	$(this).css('color', 'red');
+});
+
+$('#c1').click(function(){
+	console.log('Allo opacity');
+
+$('#c2').addClass('opacity');
+});
+
+$('#c4').click(function(){
+	console.log('slides up');
+
+	$('#c3').slideUp('slow');
+});
+
+$('#c5').click(function(){
+	console.log('slides down');
+
+	$('#c6').slideDown('slow');
+});
+
+$(document).keydown(function(){
+	console.log('remove');
+
+	$('h1').remove();
+});
+
+// $(document).keyup(function(){
+// 	console.log('another remove');
+
+// 	$('hero-unit').remove();
+// });
+
+// $('#c7').on( "click", function(){
+// 	console.log('another remove');
+
+// 	$('hero-unit').remove();
+// });
+
+$("#c7").on("click", function() {
+  alert($(this).text("hello world"));
+});
+
+$('#c1').click(function(){
+	console.log('Allo opacity');
+
+$('#c2').addClass('opacity');
+});
+
+$('#one').one("click", function(){
+	console.log('add css');
+
+$('#two').add()
+	.css("background-color", "red");
+});
+
+// $('#c2').hover(
+// 	.css("background-color", "red");
+// });
+
+$(".circa").hover(
+  function() {
+    $(this).addClass("circas");
+  }, function() {
+    $(this).removeClass("circas");
+  }
+);
+
+// $(".circa")
+//   .filter( ":odd" )
+//     .hide()
+//   .end()
+//   .filter( ":even" )
+//     .hover(function() {
+//       $( this )
+//         .toggleClass( "active" )
+//         .next()
+//           .stop( true, true )
+//           .slideToggle();
+//     });
+
+
+$('.circas').click(function(){
+	console.log('slides up');
+
+	$(this).slideToggle('slow')
+});
+
+
+
+
+
